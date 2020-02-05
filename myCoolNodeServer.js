4a351025eb06f9fc5adc642b9ca61308aee7d95f
Brodski <cbrodski@gmail.com>
2020-02-04 21:55:37 -0700

    working on https

--- myCoolNodeServer.js
+++ myCoolNodeServer.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs')
+const https = require('https') 
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -12,4 +15,21 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-app.listen(process.env.NODE_APP_PORT_NUM);
+if (process.env.NODE_ENV === 'production') {
+  
+  const privateKey = fs.readFileSync( '/etc/letsencrypt/live/customyoutube.com/privkey.pem', 'utf8')
+  const certificate = fs.readFileSync( '/etc/letsencrypt/live/customyoutube.com/cert.pem', 'utf8')
+  const ca = fs.readFileSync('/etc/letsencrypt/live/customyoutube.com/chain.pem', 'utf8')
+  const credentials = {
+    key: privateKey, 
+    cert: certificate,
+    ca: ca
+  };
+
+  const httpsServer = https.createServer(credentials, app)
+  httpsServer.listen(process.env.NODE_APP_PORT_NUM);
+  
+  
+} else {
+  app.listen(process.env.NODE_APP_PORT_NUM);
+}
