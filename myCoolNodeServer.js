39c1a2207138fa2ee18bf3df2ebdf765c88c8965
Brodski <cbrodski@gmail.com>
2020-02-06 15:28:21 -0700

    https prod

--- myCoolNodeServer.js
+++ myCoolNodeServer.js
@@ -38,11 +38,38 @@ if (process.env.NODE_APP_ENV === 'production') {
   // httpsServer.listen(443);
   https.createServer(credentials, app).listen(443)
   console.log('I assume https is running')
+
+  
   // Redirect http to https
-  http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(80);
+//   http.createServer(function (req, res) {
+//     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//     res.end();
+// }).listen(80);
+
+
+http.createServer(function (req, res) {
+  // res.writeHead(301, { "Location": "http://" + req.headers['host'] + req.url });
+  console.log("-------------------")
+  console.log("req.headers.host: ", req.headers.host)
+  console.log('req.url: ', req.url)
+  console.log("http://" + req.headers.host + req.url )
+  //let poopy = url.parse(request.url, `http://${request.headers.host}`);
+
+  // https://nodejs.org/api/http.html#http_message_url
+  // netstat -tulpn
+  let urlParse = url.parse("http://" + req.headers.host + req.url);
+  console.log(urlParse)
+  let important = "https://" + urlParse.hostname + ":" + 6969 + urlParse.path
+  console.log('important')
+  console.log(important)
+  console.log("\n\n\n\n--------\n\n\n")
+  console.log('req.headers')
+  console.log(req.headers)
+
+   res.writeHead(301, { "Location": important });
+  // res.redirect(301, "http://" + req.headers.host + req.url + ":" + 420);
+   res.end();
+}).listen(7988);
 
 console.log('I assume http (not s) is running')
 
