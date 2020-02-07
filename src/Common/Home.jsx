ea1584ae781f8b9fa8ee554485f59c5973c60fbd
Brodski <cbrodski@gmail.com>
2020-02-07 15:57:05 -0700

    about message

--- src/Common/Home.jsx
+++ src/Common/Home.jsx
@@ -9,12 +9,14 @@ function About() {
         <h1>About</h1>
         <p className="flow-text">
           It's a cool webiste that lets you organized your YouTube subscriptions!
-          You could also define the min or max duration the videos must be!
-          <br />
+          You could also define the min or max duration the videos must be, for 
+          those annoying channles that upload 30 second trailer vids when you only 
+          care about the full length video
+          {/* <br />
           <br />
           Tired of Youtube being a disorganized mess? Tired of channels that
           upload 30 second trailer vids when you only care about the full length
-          video? 
+          video?  */}
           <br />
           <br /> Custom Youtube solves that and lets you control your feed.
           <br />
