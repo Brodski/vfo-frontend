432e31df1b9e8101fa960d0230c40d5836f078ef
Brodski <cbrodski@gmail.com>
2020-02-06 17:57:49 -0700

    https in prog

--- myCoolNodeServer.js
+++ myCoolNodeServer.js
@@ -29,27 +29,27 @@ if (process.env.NODE_APP_ENV === 'production') {
     cert: certificate,
     ca: ca
   };
+  // netstat -tulpn
+  // https Springboot port
+
   // const httpsServer = https.createServer(credentials, app)
   // httpsServer.listen(443);
   https.createServer(credentials, app).listen(443)
   
-  // https Springboot port
-  https.createServer(credentials, function (req, res){
-    const redirectedPortNum = 8080
-    let urlParse = url.parse("http://" + req.headers.host + req.url);
-    let redirected = "https://" + urlParse.hostname + ":" + redirectedPortNum + urlParse.path
+  // https.createServer(credentials, function (req, res){
+  //   const redirectedPortNum = 8080
+  //   let urlParse = url.parse("http://" + req.headers.host + req.url);
+  //   let redirected = "http://" + urlParse.hostname + ":" + redirectedPortNum + urlParse.path
     
-    console.log("-------------------2")
-    console.log('redirected')
-    console.log(redirected)
-    console.log("-\n\n req -")
-    console.log(req)
-    // res.writeHead(301, { "Location": redirected });
-    // res.end();
-    res.writeHead(200)
-    res.end("HI!!!")
+  //   console.log("-------------------2")
+  //   console.log('redirected')
+  //   console.log(redirected)
 
-  }).listen(7988)
+  //   res.writeHead(301, { "Location": redirected });
+  //   res.end();
+  //   // res.writeHead(200)
+  //   // res.end("HI!!!")
+  // }).listen(7988)
 
 
   // Redirect http to https
@@ -69,26 +69,15 @@ if (process.env.NODE_APP_ENV === 'production') {
 
 } else {
   http.createServer(function (req, res) {
-    // res.writeHead(301, { "Location": "http://" + req.headers['host'] + req.url });
-    console.log("-------------------")
-    console.log("req.headers.host: ", req.headers.host)
-    console.log('req.url: ', req.url)
-    console.log("http://" + req.headers.host + req.url )
-    //let poopy = url.parse(request.url, `http://${request.headers.host}`);
-
-    // https://nodejs.org/api/http.html#http_message_url
-    // netstat -tulpn
+    
+    
     let urlParse = url.parse("http://" + req.headers.host + req.url);
-    console.log(urlParse)
-    let important = "https://" + urlParse.hostname + ":" + 6969 + urlParse.path
-    console.log('important')
-    console.log(important)
-    console.log("\n\n\n\n--------\n\n\n")
-    console.log('req.headers')
-    console.log(req.headers)
+    let redirected = "https://" + urlParse.hostname + ":" + 6969 + urlParse.path
+    console.log("-------------------")
+    console.log('redirected')
+    console.log(redirected)
 
-     res.writeHead(301, { "Location": important });
-    // res.redirect(301, "http://" + req.headers.host + req.url + ":" + 420);
+     res.writeHead(301, { "Location": redirected });
      res.end();
   }).listen(7988);
 
@@ -96,18 +85,6 @@ if (process.env.NODE_APP_ENV === 'production') {
   app.listen(4269);
   app.listen(4266);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -131,3 +108,8 @@ if (process.env.NODE_APP_ENV === 'production') {
 //   // res.redirect(301, "http://" + req.headers.host + req.url + ":" + 420);
 //    res.end();
 // }).listen(7988);
+
+
+// sudo keytool -printcert -file etc/letsencrypt/live/customyoutube.com/cert.pem 
+
+// sudo keytool -import -alias springCert -file /etc/letsencrypt/live/customyoutube.com/cert.pem -keystore keystore.p12 -storepass password
