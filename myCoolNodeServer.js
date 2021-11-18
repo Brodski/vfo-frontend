b3b102020f7c6102faf6a34f74f0a6d643ddff2c
Brodski <cbrodski@gmail.com>
2021-11-18 00:01:52 -0700

    new domain.

--- myCoolNodeServer.js
+++ myCoolNodeServer.js
@@ -17,9 +17,9 @@ app.get('/*', function(req, res) {
 
 if (process.env.NODE_APP_ENV === 'production') {
   console.log('\nWE DOING IT -' + new Date().toLocaleString())
-  const privateKey = fs.readFileSync( '/etc/letsencrypt/live/videofeedorganizer.com/privkey.pem', 'utf8')
-  const certificate = fs.readFileSync( '/etc/letsencrypt/live/videofeedorganizer.com/cert.pem', 'utf8')
-  const ca = fs.readFileSync('/etc/letsencrypt/live/videofeedorganizer.com/chain.pem', 'utf8')
+  const privateKey = fs.readFileSync( '/etc/letsencrypt/live/vfo.one/privkey.pem', 'utf8')
+  const certificate = fs.readFileSync( '/etc/letsencrypt/live/vfo.one/cert.pem', 'utf8')
+  const ca = fs.readFileSync('/etc/letsencrypt/live/vfo.one/chain.pem', 'utf8')
   const credentials = {
     key: privateKey, 
     cert: certificate,
